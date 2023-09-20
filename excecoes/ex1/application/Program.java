00b8cfbb69574c4e8920d09b2517a80aae666b45
Matheus <matheusotaviog@outlook.com>
2023-09-20 17:12:34 -0300

    Criado pasta excecoes e ex1

---
+++ excecoes/ex1/application/Program.java
@@ -0,0 +1,27 @@
+package excecoes.ex1.application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vector = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vector[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position.");
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Input error.");
+        }
+
+        System.out.println("End.");
+
+        sc.close();
+    }
+}
